3660d16c389aa5ff2db48395d2a01ea7f732682f
Aishwarya Mallampati <amallampati@google.com>
2024-07-01 20:54:46 +0000

    Add new datagram type sms
    Bug: 350514607
    Test: make
    FLAG: EXEMPT bugfix
    
    Change-Id: I69a0665274274db10d94a8725ecea9a28aeeac9b

--- satellite_client/src/android/telephony/satellite/wrapper/SatelliteManagerWrapper.java
+++ satellite_client/src/android/telephony/satellite/wrapper/SatelliteManagerWrapper.java
@@ -154,6 +154,11 @@ public class SatelliteManagerWrapper {
    * is the last message to emergency service provider indicating no more help is needed.
    */
   public static final int DATAGRAM_TYPE_LAST_SOS_MESSAGE_NO_HELP_NEEDED = 5;
+  /**
+   * Datagram type indicating that the message to be sent or received is of type SMS.
+   */
+  public static final int DATAGRAM_TYPE_SMS = 6;
+
   /** @hide */
   @IntDef(
       prefix = "DATAGRAM_TYPE_",
@@ -163,7 +168,8 @@ public class SatelliteManagerWrapper {
               DATAGRAM_TYPE_LOCATION_SHARING,
               DATAGRAM_TYPE_KEEP_ALIVE,
               DATAGRAM_TYPE_LAST_SOS_MESSAGE_STILL_NEED_HELP,
-              DATAGRAM_TYPE_LAST_SOS_MESSAGE_NO_HELP_NEEDED
+              DATAGRAM_TYPE_LAST_SOS_MESSAGE_NO_HELP_NEEDED,
+              DATAGRAM_TYPE_SMS
       })
   @Retention(RetentionPolicy.SOURCE)
   public @interface DatagramType {}
